ff1e6021c3a2da49f87011527431bc8218ce881e
sami ktami <samiktami.leadform@gmail.com>
2025-09-02 20:24:57 +0200

    Update _index.jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,9 +1,10 @@
+// app/routes/_index.jsx
 import { redirect } from "@remix-run/node";
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const to = new URL("/app", url.origin);
-  for (const [k, v] of url.searchParams.entries()) to.searchParams.set(k, v);
+  url.searchParams.forEach((v, k) => to.searchParams.set(k, v));
   return redirect(to.toString());
 };
 
