7a1876a8fa3cb19288965e52f22c661bab7e302e
sami ktami <samiktami.leadform@gmail.com>
2025-09-05 16:16:44 +0200

    Update app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,14 +1,8 @@
 // app/routes/app.jsx
 import { Outlet } from "@remix-run/react";
-import { AppProvider } from "@shopify/polaris";
-import en from "@shopify/polaris/locales/en.json";
-import "@shopify/polaris/build/esm/styles.css";
 
 
 export default function AppLayout() {
-  return (
-    <AppProvider i18n={en}>
-      <Outlet />
-    </AppProvider>
-  );
+  // Le AppProvider est déjà appliqué globalement dans root.jsx
+  return <Outlet />;
 }
