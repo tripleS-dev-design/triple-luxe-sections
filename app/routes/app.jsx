6b3d00f2ee9c7d52fff844cc6432ab16fee48d77
sami ktami <samiktami.leadform@gmail.com>
2025-09-05 07:11:35 +0200

    Update app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,9 +1,13 @@
-// app/app/routes/app.jsx
-import React from "react";
+// app/routes/app.jsx
 import { Outlet } from "@remix-run/react";
+import { AppProvider } from "@shopify/polaris";
+import en from "@shopify/polaris/locales/en.json";
 import "@shopify/polaris/build/esm/styles.css";
 
-export default function App() {
-  // Pas de nav ici → plus de "HomePricing"
-  return <Outlet />;
+export default function AppLayout() {
+  return (
+    <AppProvider i18n={en}>
+      <Outlet />
+    </AppProvider>
+  );
 }
