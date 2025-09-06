1c73f8a29c7fd3c4af7583a08a2a597139c411cc
sami ktami <samiktami.leadform@gmail.com>
2025-09-06 23:08:06 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -1,14 +1,4 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  LiveReload,
-} from "@remix-run/react";
-import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
-
-export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
 export default function Root() {
   return (
@@ -16,14 +6,11 @@ export default function Root() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <Meta /><Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
+        <ScrollRestoration /><Scripts />
       </body>
     </html>
   );
