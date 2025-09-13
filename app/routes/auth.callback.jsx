673226944da3126143dc4d27caa0f6f9a9cb82a6
sami ktami <samiktami.leadform@gmail.com>
2025-09-13 22:51:10 +0200

    Delete app/routes/auth.callback.jsx

--- app/routes/auth.callback.jsx
+++
@@ -1,7 +0,0 @@
-// app/routes/auth.callback.jsx
-export const loader = async ({ request }) => {
-  const { authenticate } = await import("../shopify.server");
-  // Gère le callback OAuth et renvoie vers /app
-  return authenticate.admin(request);
-};
-export default function AuthCallback(){ return null; }
