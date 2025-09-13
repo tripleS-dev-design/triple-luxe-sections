161a6a0eb3747ce4e12cb9a68c4e1ff29b9e544e
sami ktami <samiktami.leadform@gmail.com>
2025-09-14 00:09:12 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -1,7 +1,7 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteError } from "@remix-run/react";
-import { boundary } from "@shopify/shopify-app-remix/server";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { addDocumentResponseHeaders } from "./shopify.server";
 
-export const headers = (h) => boundary.headers(h);
+export const headers = (args) => addDocumentResponseHeaders(args);
 
 export default function Root() {
   return (
@@ -9,15 +9,14 @@ export default function Root() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://cdn.shopify.com/" />
-        <link rel="stylesheet" href="https://cdn.shopify.com/static/fonts/inter/v4/styles.css" />
-        <Meta /><Links />
+        <Meta />
+        <Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration /><Scripts />
+        <ScrollRestoration />
+        <Scripts />
       </body>
     </html>
   );
 }
-export function ErrorBoundary(){ return boundary.error(useRouteError()); }
