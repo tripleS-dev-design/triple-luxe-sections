001ee2c45af61adc673d7f4ff0e7b848e31526aa
sami ktami <samiktami.leadform@gmail.com>
2025-09-13 22:55:45 +0200

    Delete app/routes/auth.login.jsx

--- app/routes/auth.login.jsx
+++
@@ -1,9 +0,0 @@
-// app/routes/auth.login.jsx
-export const loader = async ({ request }) => {
-  const { login } = await import("../shopify.server");
-  return login(request);
-};
-
-export default function AuthLogin() {
-  return null;
-}
