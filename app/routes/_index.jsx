cdaa942cddbcedb387cb7027009a65d09a04e9ee
sami ktami <samiktami.leadform@gmail.com>
2025-09-14 17:24:30 +0200

    Update _index.jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,8 +1,9 @@
+// app/routes/_index.jsx
 import { redirect } from "@remix-run/node";
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
   const shop = url.searchParams.get("shop") || process.env.SHOPIFY_DEV_SHOP;
   if (shop) return redirect(`/app?shop=${shop}`);
-  return redirect("/auth/login"); // fallback si pas de shop connu
+  return redirect("/auth/login");
 };
