f8e5e215aa935726b698c99575574e43394b3c78
sami ktami <samiktami.leadform@gmail.com>
2025-09-14 16:20:59 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -1,9 +1,8 @@
 // app/root.jsx
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
-// Enlève l'import addDocumentResponseHeaders et utilise ce headers simple
+// ⛔️ NE PAS importer addDocumentResponseHeaders
 export const headers = () => ({
-  // Autorise l’embed dans l’admin et les boutiques Shopify
   "Content-Security-Policy":
     "frame-ancestors https://admin.shopify.com https://*.myshopify.com;",
 });
@@ -14,13 +13,11 @@ export default function Root() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <Meta /><Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        <ScrollRestoration /><Scripts />
       </body>
     </html>
   );
