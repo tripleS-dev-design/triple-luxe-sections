35b1531cdd3475f615e97abb5b2a3de3b3b28b0a
sami ktami <samiktami.leadform@gmail.com>
2025-09-15 14:13:42 +0200

    Update app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,5 +1,4 @@
-// app/routes/app.jsx
-import React from "react";
+import { json } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
 import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
 import polarisTranslations from "@shopify/polaris/locales/en.json";
@@ -13,7 +12,7 @@ export const loader = async ({ request }) => {
   const shop = session.shop; // ex: "selya-store.myshopify.com"
   const shopSub = shop.replace(".myshopify.com", "");
 
-  return Response.json({
+  return json({
     shopSub,
     apiKey: process.env.SHOPIFY_API_KEY || "",
   });
