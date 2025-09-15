44f3961768eebcc5e727c54bb5ab786e2a24f380
sami ktami <samiktami.leadform@gmail.com>
2025-09-16 00:39:52 +0200

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,6 @@
 // app/routes.js
 import { flatRoutes } from "@remix-run/fs-routes";
 
-/**
- * Remix va appeler cette fonction au build pour générer la table des routes.
- * On lui dit simplement d'utiliser le dossier "app/routes".
- */
 export default function routes(defineRoutes) {
-  // équivaut à la config par défaut, mais explicite
   return flatRoutes("routes", defineRoutes);
 }
