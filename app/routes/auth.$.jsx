fb7e94219f76146dc10ec87ed20e050bb419f0e1
sami ktami <samiktami.leadform@gmail.com>
2025-09-21 06:36:02 +0200

    Update auth.$.jsx

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,11 +1,7 @@
 // app/routes/auth.$.jsx
-import { redirect } from "@remix-run/node";
-import { shopify, registerWebhooks } from "../shopify.server";
+import { authenticate } from "../shopify.server";
 
-export async function loader({ request }) {
-  const { session } = await shopify.authenticate.admin(request);
-  // Inscrire/mettre à jour toutes les subscriptions déclarées dans shopify.server.js
-  await registerWebhooks({ session });
-  // redirige vers ton UI embarquée
-  return redirect("/app");
-}
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+  return null;
+};
