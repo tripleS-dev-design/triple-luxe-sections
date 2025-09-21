f9efa64c58b2d143b7aee28a71fc5905c3735014
sami ktami <samiktami.leadform@gmail.com>
2025-09-21 06:35:44 +0200

    Update app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,31 +1,53 @@
-import { json } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
+// app.jsx
+import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
-import fr from "@shopify/polaris/locales/fr.json";
-import styles from "@shopify/polaris/build/esm/styles.css?url";
-export const links = () => [{ rel: "stylesheet", href: styles }];
+import enTranslations from "@shopify/polaris/locales/en.json";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { NavMenu } from "@shopify/app-bridge-react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+import { json } from "@remix-run/node";
+
+export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }) => {
-  const appHandle = "triple-luxe-sections";
   const { authenticate } = await import("../shopify.server");
   const { billing, redirect, session } = await authenticate.admin(request);
 
+  // Managed Pricing: vérifie si un abonnement actif existe
   const { hasActivePayment } = await billing.check();
+
+  // Si pas d’abonnement → page Pricing Plans (admin)
   if (!hasActivePayment) {
+    const appHandle = "triple-luxe-sections"; // = handle de ton shopify.app.toml
     const storeHandle = session.shop.replace(".myshopify.com", "");
     return redirect(
       `https://admin.shopify.com/store/${storeHandle}/charges/${appHandle}/pricing_plans`,
-      { target: "_top" }
+      { target: "_top" } // sortir de l’iframe
     );
   }
+
   return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
 };
 
-export default function AppLayout() {
+export default function App() {
   const { apiKey } = useLoaderData();
   return (
-    <PolarisAppProvider i18n={fr}>
-      <Outlet context={{ apiKey }} />
+    <PolarisAppProvider i18n={enTranslations}>
+      <AppProvider isEmbeddedApp apiKey={apiKey}>
+        <NavMenu>
+          <Link to="/premium">Premium Interface</Link>
+        </NavMenu>
+        <Outlet />
+      </AppProvider>
     </PolarisAppProvider>
   );
 }
+
+export function ErrorBoundary() {
+  return boundary.error(useRouteError());
+}
+
+export const headers = (headersArgs) => {
+  return boundary.headers(headersArgs);
+};
