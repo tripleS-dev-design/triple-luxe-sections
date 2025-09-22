fc44e811d16a3f6cec07ad8bce2d2f021e39ce15
sami ktami <samiktami.leadform@gmail.com>
2025-09-23 01:24:13 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -11,7 +11,7 @@ export const headers = () => {
 
 export default function Root() {
   return (
-    <html lang="fr">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
