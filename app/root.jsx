b0bf2eb188183572acca84d913a741fc37757656
sami ktami <samiktami.leadform@gmail.com>
2025-09-28 05:45:27 +0200

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -1,25 +1,18 @@
-// app/root.jsx
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
-
 export const headers = () => ({
   "Content-Security-Policy": "frame-ancestors https://admin.shopify.com https://*.myshopify.com;",
   "Referrer-Policy": "origin-when-cross-origin",
   "Cache-Control": "no-store",
 });
-
 export default function Root() {
   return (
     <html lang="fr">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
+        <meta charSet="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta /><Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
+        <Outlet /><ScrollRestoration /><Scripts />
       </body>
     </html>
   );
